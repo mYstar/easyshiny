7ad2f16202822eca3d56a27f62e7bae2a5985db1
Eric Starke <eric_st@web.de>
2019-03-18 14:47:49 +0100

    test: es_add_output

--- tests/testthat/test-es_add_output.R
+++ tests/testthat/test-es_add_output.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(tibble)
 library(checkmate)
+library(ggplot2)
+library(vdiffr)
 
 context('test adding output elements to the easyshiny app')
 
@@ -27,4 +29,58 @@ test_that('output objects are inserted correctly', {
   expect_equal(last_elem$box, 'resIMG')
   expect_list(test_func(), min.len = 1)
   expect_subset( 'src', names(test_func()) )
+
+  # render Plot
+  test_func <- es_renderPlot({qplot(mtcars$mpg)}, tab = 'testPlot', box = 'resPlot')
+  visuals <- get('visuals', envir = easyshiny:::appData)
+  last_elem <- visuals[length(visuals[,1]),]
+
+  expect_string(last_elem$id)
+  expect_equal(last_elem$tab, 'testPlot')
+  expect_equal(last_elem$box, 'resPlot')
+  expect_class(test_func(), classes = c('gg', 'ggplot'))
+  expect_doppelganger(title = 'mpg test', test_func())
+
+  # render Print
+  test_func <- es_renderPrint({'This is a printable text'}, tab = 'testPrint', box = 'resPrint')
+  visuals <- get('visuals', envir = easyshiny:::appData)
+  last_elem <- visuals[length(visuals[,1]),]
+
+  expect_string(last_elem$id)
+  expect_equal(last_elem$tab, 'testPrint')
+  expect_equal(last_elem$box, 'resPrint')
+  expect_equal('This is a printable text', test_func())
+
+  # render Table
+  test_func <- es_renderTable({as.tibble(mtcars)}, tab = 'testTable', box = 'resTable')
+  visuals <- get('visuals', envir = easyshiny:::appData)
+  last_elem <- visuals[length(visuals[,1]),]
+
+  expect_string(last_elem$id)
+  expect_equal(last_elem$tab, 'testTable')
+  expect_equal(last_elem$box, 'resTable')
+  expect_tibble(test_func())
+
+  # render Text
+  text_example <- 'This is a printable text!'
+  test_func <- es_renderText({text_example}, tab = 'testText', box = 'resText')
+  visuals <- get('visuals', envir = easyshiny:::appData)
+  last_elem <- visuals[length(visuals[,1]),]
+
+  expect_string(last_elem$id)
+  expect_equal(last_elem$tab, 'testText')
+  expect_equal(last_elem$box, 'resText')
+  expect_equal(text_example, test_func())
+
+  # render UI
+  html_example <- tags$h1('This is HTML')
+  test_func <- es_renderUI({html_example}, tab = 'testUI', box = 'resUI')
+  visuals <- get('visuals', envir = easyshiny:::appData)
+  last_elem <- visuals[length(visuals[,1]),]
+
+  expect_string(last_elem$id)
+  expect_equal(last_elem$tab, 'testUI')
+  expect_equal(last_elem$box, 'resUI')
+  expect_class(test_func(), classes = 'shiny.tag')
+  expect_equal(html_example, test_func())
 } )
